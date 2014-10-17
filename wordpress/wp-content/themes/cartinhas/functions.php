fa02fd7293bb9045965d51258c5cafd2cbd9bfd6
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-10-16 21:37:40 -0300

    agora sim, começa um tema novo, filho de um tema que usa bootstrap

--- wordpress/wp-content/themes/cartinhas/functions.php
+++ wordpress/wp-content/themes/cartinhas/functions.php
@@ -2,3 +2,10 @@
 
 // desliga a barra de admin do wordpress
 add_filter('show_admin_bar', '__return_false');
+
+// cuida da herança de estilos do tema pai
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+function enqueue_child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
+}
