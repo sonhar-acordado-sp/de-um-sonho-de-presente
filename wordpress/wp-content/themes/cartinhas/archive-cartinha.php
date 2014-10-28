54abac2c5d8d59cfca5f2c5ef9554b3ce2040230
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-10-28 07:50:03 -0200

    comecei a pensar na galeria de cartinhas

--- wordpress/wp-content/themes/cartinhas/archive-cartinha.php
+++ wordpress/wp-content/themes/cartinhas/archive-cartinha.php
@@ -12,7 +12,7 @@
 
                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                    <div class="col-sm-4" id="post-<?php the_ID(); ?>">
+                    <div class="col-sm-4 cartinha" id="post-<?php the_ID(); ?>">
                         <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
                             <?php the_post_thumbnail('medium'); ?>
                         </a>
