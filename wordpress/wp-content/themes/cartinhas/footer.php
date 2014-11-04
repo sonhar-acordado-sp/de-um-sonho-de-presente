4d8015706ca2e500965cfc20b7820c5e282f1043
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-11-04 11:21:55 -0200

    conserta footer

--- wordpress/wp-content/themes/cartinhas/footer.php
+++ wordpress/wp-content/themes/cartinhas/footer.php
@@ -1,24 +1,24 @@
-            <footer role="contentinfo">
-
-                <div id="inner-footer" class="clearfix">
-                  <hr />
-                  <div id="widget-footer" class="clearfix row">
-                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-                    <?php endif; ?>
-                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-                    <?php endif; ?>
-                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-                    <?php endif; ?>
-                  </div>
-
-                    <nav class="clearfix">
-                        <?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-                    </nav>
-                </div> <!-- end #inner-footer -->
-
-            </footer> <!-- end footer -->
-
         </div> <!-- end #container -->
+
+        <footer role="contentinfo" class="clearfix">
+            <br/>
+            <div id="inner-footer">
+              <div id="widget-footer" class="clearfix row">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+                <?php endif; ?>
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+                <?php endif; ?>
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+                <?php endif; ?>
+              </div>
+
+                <nav class="clearfix">
+                    <?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+                </nav>
+            </div> <!-- end #inner-footer -->
+
+        </footer> <!-- end footer -->
+
 
         <!--[if lt IE 7 ]>
             <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
